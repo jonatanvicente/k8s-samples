3928ec415e58f4ed858275079ba9299090ab10b2
Jonatan Vicente <it@jonatanvicente.com>
2025-05-02 14:10:04 +0200

    Testing Access using temp pod

---
+++ k8s-hands-on_v3/backend/src/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	//"fmt"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+type HandsOn struct {
+	Time 		time.Time 	`json:"time"`
+	Hostname 	string		`json:"hostname"`
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	//w.Write([]byte(`{"message": "Hello World"}`))
+	//resp := fmt.Sprintf("La hora es %v y hostname es %v", time.Now(), os.Getenv("HOSTNAME"))
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	resp := HandsOn{
+		Time: 		time.Now(),
+		Hostname: 	os.Getenv("HOSTNAME"),
+	}
+	jsonResp, err := json.Marshal(&resp)
+	if(err != nil){
+		w.Write([]byte("Error"))
+		return
+	}
+	//w.Write([]byte(resp))
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResp)
+}
+
+func main(){
+	http.HandleFunc("/", ServeHTTP)
+	http.ListenAndServe(":9090", nil)
+}
